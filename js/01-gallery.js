8f43d3f305093b2fbec705d110a4c1b6404d79ff
NadyaKonzevaya <borisova.nadya@gmail.com>
2022-04-27 16:35:06 +0300

    добавляет 1 и 2 задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,114 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// Задание 1. Создай галерею с возможностью клика по её элементам и просмотра полноразмерного изображения в модальном окне. 
+// Посмотри демо видео работы галереи.
 
-console.log(galleryItems);
+
+// +1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+// +2. Реализация делегирования на div.gallery и получение url большого изображения.
+// +3. Подключение скрипта и стилей библиотеки модального окна basicLightbox.
+//  +Используй CDN сервис jsdelivr и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
+// +4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+// +5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
+
+// Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox.
+// Разметка элемента галереи
+// Ссылка на оригинальное изображение должна храниться в data-атрибуте source на элементе <img>, и указываться в href ссылки. 
+// Не добавляй другие HTML теги или CSS классы кроме тех, что есть в этом шаблоне.
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+// +Обрати внимание на то, что изображение обернуто в ссылку, 
+// +а значит при клике по умолчанию пользователь будет перенаправлен на другую страницу. 
+// +Запрети это поведение по умолчанию.
+
+// Закрытие с клавиатуры
+// ⚠️ Следующий функционал не обязателен при сдаче задания, но будет хорошей дополнительной практикой.
+
+// Добавь закрытие модального окна по нажатию клавиши Escape. 
+// Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. 
+// У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+
+// Создаем рефы
+const refs = {
+    gallery : document.querySelector(".gallery"),
+}
+
+// создаем разметку по массиву данных
+
+function createItemsMarkup (items) {
+    return items.map(item => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${item.original}>
+                <img
+                    class="gallery__image"
+                    src=${item.preview}
+                    data-source=${item.original}
+                    alt=${item.description}
+                />
+            </a>
+        </div>
+    `}).join("");
+};
+
+const markup = createItemsMarkup(galleryItems);
+
+
+// рендерим разметку
+
+refs.gallery.innerHTML = markup;
+
+// Реализация делегирования на div.gallery и получение url большого изображения
+
+refs.gallery.addEventListener("click", onImageClick);
+
+function onImageClick (event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    
+    
+    event.target.src = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img width="1400" height="900" src=${event.target.src}>
+    `,
+   //  добавляем закрытие модального окна по нажатию клавиши Escape и прослушивание клавиатуры было только пока открыто модальное окно
+    {onShow: (instance) => {window.addEventListener("keydown", onEscapeKeyPress)},
+	onClose: (instance) => {window.removeEventListener("keydown", onEscapeKeyPress)}});
+    instance.show();
+
+    function onEscapeKeyPress (event) {
+        if (event.code === "Escape" ) {
+            instance.close();
+        }
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,73 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+// Задание 2 - библиотека SimpleLightbox
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, 
+// которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, 
+// а также пролистывание изображений при помощи клавиатуры.
+// Посмотри демо видео работы галереи с подключенной библиотекой.
+
+// Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
+
+// <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// +1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. 
+// Используй готовый код из первого задания.
+// +2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. 
+// Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// +3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+// Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// +4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt.
+//  Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+
+
+// Создаем рефы
+const refs = {
+    gallery : document.querySelector(".gallery"),
+}
+
+// создаем разметку по массиву данных
+
+function createItemsMarkup (items) {
+    return items.map(item => {
+        return `
+        <a class="gallery__item" href=${item.original}>
+            <img class="gallery__image" src=${item.preview} alt=${item.description} />
+        </a>
+    `}).join("");
+};
+
+const markup = createItemsMarkup(galleryItems);
+
+// рендерим разметку
+
+refs.gallery.innerHTML = markup;
+
+// Реализация делегирования на div.gallery и получение url большого изображения
+
+refs.gallery.addEventListener("click", onImageClick);
+
+function onImageClick (event) {
+    event.preventDefault();
+    console.log()
+    
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+// добавить отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        caption: true, 
+        captionSelector: 'img',
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    })
+}
+
